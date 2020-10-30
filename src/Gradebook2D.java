528f29061a132642c0c278d53de5998486d6ce63
guidoasbun <gdrodrigo8@gmail.com>
2020-10-29 23:37:20 -0700

    Part B comments

--- src/Gradebook2D.java
+++ src/Gradebook2D.java
@@ -12,13 +12,16 @@ public class Gradebook2D {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        //gets input of how many users
+        //inits array of id, and 2D array of quizzes on numStudents
         System.out.print("How many students in the class? ");
         int numStudents = in.nextInt();
         int[] id = new int[numStudents];
         double[][] quizzes = new double[numStudents][3];
-        double quizSum = 0.0;
+        double quizzSum = 0.0;
         double classAverage;
 
+        //user inputs for both arrays
         for(int i = 0; i < numStudents; i++){
             System.out.print("Enter student ID: ");
             id[i] = in.nextInt();
@@ -29,6 +32,8 @@ public class Gradebook2D {
             }
         }
 
+        //calculates and prints out the Final grade average for every student
+        //adds students final average to the total class sum of grades
         for(int i = 0; i < numStudents; i++){
             double stuAverage;
             double stuSum = 0.0;
@@ -37,12 +42,13 @@ public class Gradebook2D {
                 stuSum += quizzes[i][j];
             }
             stuAverage = stuSum / 3;
-            quizSum += stuAverage;
+            quizzSum += stuAverage;
             System.out.print("\nID " + id[i] + " - Final grade: ");
             System.out.printf("%.2f", stuAverage);
         }
 
-        classAverage = quizSum/numStudents;
+        //calculates the class average and prints it out
+        classAverage = quizzSum/numStudents;
         System.out.print("\nThe class average: ");
         System.out.printf("%.2f", classAverage);
 
